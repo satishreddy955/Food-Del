73d9b2370ff0dcefbb76b3c54acf7eb856d02eb1
satishreddy955 <168215588+satishreddy955@users.noreply.github.com>
2025-08-11 18:43:06 +0530

    Update assets.js

--- frontend/src/admin/assets/assets.js
+++ frontend/src/admin/assets/assets.js
@@ -5,7 +5,7 @@ import profile_image from './profile_image.png'
 import upload_area from './upload_area.png'
 import parcel_icon from './parcel_icon.png'
 
-export const url = 'https://food-del-backend-3pfs.onrender.com'
+export const url = 'https://food-del-backend-3pfs.onrender.com/api'
 export const currency = '₹'
 
 export const assets ={
